0292ad35026e27aefa3fa954f7c1d7e2f6f9fba1
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-05-10 15:56:08 +0530

    introduction

---
+++ basic/basic1.js
@@ -0,0 +1,42 @@
+// console.log("Hello world...");
+// console.info("information function...");
+// console.warn("warning functon...");
+// console.error("error function..");
+
+// Datatype : number , string , boolean , undefined , object
+
+
+a = 10;
+console.log("value of a = ",a);
+// console.log("value of a = " + a);
+console.log("type of a =", typeof(a));
+
+
+b = 12.44;
+console.log("value of b = ",b);
+console.log("type of b = ",typeof b);
+
+c = 'kunal';
+console.log("value of c = ",c);
+console.log("type of c = ",typeof c);
+
+isAlive = true;
+console.log("value of isAlive = ",isAlive);
+console.log("type of isAlive = ",typeof isAlive);
+
+let d;
+console.log("value of d = ",d);
+console.log("type of d = ",typeof d);
+
+let e = null;
+console.log("value of e = ",e);
+console.log("type of e = ",typeof e);
+
+
+
+
+
+
+
+
+
