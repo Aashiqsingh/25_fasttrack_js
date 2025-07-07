9a35e9771813ff40d014ba69ca206213ddd3f065
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-07-07 17:33:11 +0530

    date

---
+++ Date/date.js
@@ -0,0 +1,36 @@
+const getDate = ()=>{
+
+    // const date = new Date();
+
+    // // console.log(date);
+    // console.log("year ---> ",date.getFullYear());
+    // console.log("month ---> ",date.getMonth() + 1);
+    // console.log("day ---> ",date.getDate());
+    // console.log("hour ---> ",date.getHours());
+    // console.log("minute ---> ",date.getMinutes());
+    // console.log("second ---> ",date.getSeconds());
+    // console.log("milliseconds ---> ",date.getMilliseconds());
+    // console.log("weekday ---> ",date.getDay());
+    // console.log(date.getTime());
+    
+    
+
+
+    const date2 = new Date();
+    date2.setDate(14);
+    date2.setMonth(1);
+    date2.setFullYear(2020);
+    date2.setHours(11);
+    date2.setMinutes(12);
+    date2.setSeconds(10);
+
+
+    console.log(date2);
+    
+
+    
+
+}
+
+
+getDate()
---
+++ Watch/clock.js
@@ -0,0 +1,28 @@
+function getTime(){
+
+    const date = new Date();
+
+    let hour = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
+
+
+    let hours = document.getElementById("hours");
+    let mins = document.getElementById("mins");
+    let secs = document.getElementById("secs");
+
+
+    hours.innerHTML = hour < 10 ? "0" + hour : hour;
+    mins.innerHTML = min < 10 ? "0" + min : min;
+    secs.innerHTML = sec < 10 ? "0" + sec : sec;
+
+
+
+    secs.map((e))
+
+
+}
+setInterval(()=>{
+    getTime();
+
+},1000)
